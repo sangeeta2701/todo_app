a179641f67a1b47c1644a42c6d81fc86983b6500
DELL <anjugupta7060@gmail.com>
2023-08-15 16:00:56 +0530

    Todo  model

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/provider/todos.dart';
 import 'package:todo_app/screens/home_page.dart';
 
 
@@ -12,14 +14,17 @@ static final String title = "Todo App";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: title,
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        scaffoldBackgroundColor: Color(0xFFf6f5ee)
+    return ChangeNotifierProvider(
+      create: (context)=> TodosProvider(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          scaffoldBackgroundColor: Color(0xFFf6f5ee)
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
